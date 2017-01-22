2d51602d5c5dd0babe9f895b7c81d3508b52c51b
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-01-22 13:01:04 +0100

    Split the section of the band into a custom directive

--- src/main/webapp/javascript/directives.js
+++ src/main/webapp/javascript/directives.js
@@ -6,4 +6,12 @@ angular.module('onirianApp', [])
             restrict: 'E',
             templateUrl: 'templates/components/concerts.html'
         }
+    })
+
+    // include the 'band.html' into the <band> tag
+    .directive('band', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/components/band.html'
+        }
     });
