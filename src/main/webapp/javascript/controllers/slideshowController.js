59f963d96071745f21ed95bae4139c5dc240eed3
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-01-22 17:33:46 +0100

    Add automatic slideshow

--- src/main/webapp/javascript/controllers/slideshowController.js
+++ src/main/webapp/javascript/controllers/slideshowController.js
@@ -1,18 +1,35 @@
 angular.module('onirianApp')
 
-    .controller('slideshowCtrl', ['$scope', function ($scope) {
+    .controller('slideshowCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
 
-        $scope.predeterminedExercises = [];    // predetermined exercises
-        $scope.predeterminedMG = [];    // predetermined muscle groups
-        $scope.predeterminedCardioExercises = [];    // predetermined cardio exercises
-        $scope.predeterminedCardioTypes = [];    // predetermined cardio types
+        var slideIndex = 1;
+        var timer;
 
-        $scope.getPredetermined = function () {
-            exerciseService.getPredetermined(function (predeterminedMG,predeterminedExercises, cardioTypes, cardioExercises) {
-                $scope.predeterminedMG = predeterminedMG;
-                $scope.predeterminedExercises = predeterminedExercises;
-                $scope.predeterminedCardioTypes = cardioTypes;
-                $scope.predeterminedCardioExercises = cardioExercises;
-            },showError);
+        $scope.plusSlides = function (n) {
+            $scope.showSlides(slideIndex += n);
         };
+
+        $scope.currentSlide = function (n) {
+            $scope.showSlides(slideIndex = n);
+        };
+
+        $scope.showSlides = function (n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+            $timeout.cancel(timer);
+            timer = $timeout(function() {
+                $scope.plusSlides(1);}, 4000); // Change image every 4 seconds
+        };
+        $scope.showSlides(slideIndex);
     }]);
