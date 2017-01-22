04f7d3bbe3d59845eefc6fe92aa7ba4415ef2d32
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-01-22 03:48:53 +0100

    Correct minor fixes

--- src/main/webapp/javascript/directives.js
+++ src/main/webapp/javascript/directives.js
@@ -1,28 +1,9 @@
-angular.module('onirianApp')
-
-    // include the 'navbar.html' into the <navbar> tag
-    .directive('navbar', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/components/navbar.html',
-            controller: 'navbarCtrl'
-        }
-    })
+angular.module('onirianApp', [])
 
     // include the 'concerts.html' into the <concerts> tag
     .directive('concerts', function () {
         return {
             restrict: 'E',
-            templateUrl: 'templates/components/concerts.html',
-            controller: 'recordCtrl',
-            scope: true
-        }
-    })
-
-    //include the 'exerciserCard.html' into the <exerciserCard> tag
-    .directive('band', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/components/band.html'
+            templateUrl: 'templates/components/concerts.html'
         }
     });
