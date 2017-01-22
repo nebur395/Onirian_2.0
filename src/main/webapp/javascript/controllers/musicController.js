046a3790285f4da02ea7f20c995ac6383bc96e5b
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-01-22 22:37:46 +0100

    Modify elements id to improve accesibility

--- src/main/webapp/javascript/controllers/musicController.js
+++ src/main/webapp/javascript/controllers/musicController.js
@@ -3,7 +3,7 @@ angular.module('onirianApp')
     .controller('musicCtrl', ['$scope', function ($scope) {
 
         // When the user clicks anywhere outside of the modal, close it
-        var modal = document.getElementById('ticketModal');
+        var modal = document.getElementById('musicModal');
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
